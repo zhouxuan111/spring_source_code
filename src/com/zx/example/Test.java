abb130a761347572114712f21b0c7dfa3b6f44af
zhouxuan19 <xuanzhou19@creditease.cn>
2020-08-14 18:00:33 +0800

    优化

--- src/com/zx/example/Test.java
+++ src/com/zx/example/Test.java
@@ -1,6 +1,5 @@
 package com.zx.example;
 
-import com.zx.aspect.UserService;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -13,11 +12,12 @@ public class Test {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
 
-        /*TestCglib userService = (TestCglib) context.getBean("testCglib");
-        userService.test();*/
+       /* TestCglib userService = (TestCglib) context.getBean("testCglib");
+        userService.test();
 
         UserService userService = (UserService) context.getBean("userService");
 
-        userService.add();
+        userService.add();*/
+
     }
 }
